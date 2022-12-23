7e619e78420dc83050fbf70fead0711996a02ed4
Andreas Wänqvist <andrkarl@live.com>
2022-12-23 14:50:13 +0100

    new component

---
+++ header-component/header-component2.js
@@ -0,0 +1,50 @@
+class HeaderComponent2 extends HTMLElement {
+
+    _text;
+    get text() {
+        // this._text = this.getAttribute('text') || '';
+        // return this._text;
+
+        return this.getAttribute('text') || '';
+    }
+
+    set text(value) {
+        // this._text = value;
+    }
+
+    constructor() {
+        console.log("Constructor for header-component started");
+
+        // Always call super first in constructor
+        super();
+
+        // Create a shadow root
+        const shadow = this.attachShadow({ mode: 'open' });
+
+        // Create header
+        const header = document.createElement('h1');
+
+        // Take attribute content and put it inside the info span
+        // const text = this.getAttribute('data-text');
+        header.textContent = this.text;
+
+        // Create some CSS to apply to the shadow dom
+        console.log("setting up style");
+        const style = document.createElement('style');
+
+        style.textContent = `
+         h1 {
+            color: green;
+         }
+        `;
+        shadow.appendChild(style);
+
+        // Attach the created elements to the shadow dom
+        shadow.appendChild(header);
+    }
+}
+
+// Define the new element
+customElements.define('header-component2', HeaderComponent2);
+
+export default HeaderComponent2;
