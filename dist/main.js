2ffe622031dd490e652cc3c7039caf96a7e95eb5
Andreas Wänqvist <andrkarl@gmail.com>
2022-12-26 11:48:02 +0100

    moved main

---
+++ dist/main.js
@@ -0,0 +1,4 @@
+import HeaderComponent from "./header-component/header-component.js";
+import HeaderComponent2 from "./header-component/header-component2.js";
+export default HeaderComponent;
+export { HeaderComponent2 };
