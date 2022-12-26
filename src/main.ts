cbb7c37684ad9bc5dc03b929daf79969e094ce89
Andreas Wänqvist <andrkarl@gmail.com>
2022-12-26 11:52:30 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import HeaderComponent from "./header-component/header-component.js";
 import HeaderComponent2 from "./header-component/header-component2.js";
 
-
-export default HeaderComponent;
+export { HeaderComponent };
 export { HeaderComponent2 };
