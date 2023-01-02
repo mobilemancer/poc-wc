19f6c6a9ba4b19f3a808afd5e1866bb4957ad991
Andreas Wänqvist <andrkarl@live.com>
2023-01-02 11:15:02 +0100

    fix: replacement suffix

--- src/base/TemplateParser.test.ts
+++ src/base/TemplateParser.test.ts
@@ -1,17 +1,29 @@
 import TemplateParser from "./TemplateParser";
 
-let template = '<div onclick="clicked()">apa</div>';
 let template2 = "<div>${test}</div>";
 let template2expected = '<div><span id="test0"></span></div>';
 
-describe("accordion", () => {
-  it("parse events", () => {
-    const result = TemplateParser.parse(template);
-    expect(result).toBe(template);
-  });
+let template3 = "<p>tihs is a pretty booring ${test}, but it ${test}s a lot</p>";
+let template3expected = '<p>tihs is a pretty booring <span id="test1"></span>, but it <span id="test2"></span>s a lot</p>';
+
+let template4 = "<p>tihs is a pretty booring ${xyz}, but it ${xyz}s a lot</p>";
+let template4expected = '<p>tihs is a pretty booring <span id="xyz0"></span>, but it <span id="xyz1"></span>s a lot</p>';
+
+describe("string literal parsing", () => {
 
   it("parse string literals", () => {
     const result = TemplateParser.parse(template2);
     expect(result).toBe(template2expected);
   });
+
+  it("parse string literals, multi instance with used template", () => {
+    const result = TemplateParser.parse(template3);
+    expect(result).toBe(template3expected);
+  });
+
+  it("parse string literals, multi instance with unused template", () => {
+    const result = TemplateParser.parse(template4);
+    expect(result).toBe(template4expected);
+  });
+
 });
--- src/base/TemplateParser.ts
+++ src/base/TemplateParser.ts
@@ -35,35 +35,33 @@ export default class TemplateParser {
   static stringLiteralCounter = 0;
   static stringLiteralReplacements = new Map<string, string[]>();
   static findStringLiterals(elements: Element[]): Element[] {
-    elements.forEach((element) => {
-      if (element.innerHTML.includes("${")) {
-        let index = 0;
-        while (element.innerHTML.indexOf("${", index) > -1) {
-          let start = element.innerHTML.indexOf("${", index);
-          let end = element.innerHTML.indexOf("}", start);
-          let stringLiteral = element.innerHTML.substring(start, end + 1);
-          let stringLiteralName = element.innerHTML.substring(start + 2, end);
+    elements.filter(element => element.innerHTML.includes("${")).forEach(element => {
+      let end = 0;
+      while (element.innerHTML.indexOf("${", end) > -1) {
+        let start = element.innerHTML.indexOf("${", end);
+        end = element.innerHTML.indexOf("}", start);
+        let stringLiteral = element.innerHTML.substring(start, end + 1);
+        let stringLiteralName = element.innerHTML.substring(start + 2, end);
 
-          element.innerHTML = element.innerHTML.replace(
-            stringLiteral,
-            `<span id='${
-              stringLiteralName + this.stringLiteralCounter++
-            }'></span>`
-          );
-
-          if (!this.stringLiteralReplacements.has(stringLiteralName)) {
-            this.stringLiteralReplacements.set(stringLiteralName, []);
-          }
-          this.stringLiteralReplacements
-            .get(stringLiteralName)
-            ?.push(stringLiteralName + this.stringLiteralCounter);
-
-          index = end + 1;
+        if (!this.stringLiteralReplacements.has(stringLiteralName)) {
+          this.stringLiteralReplacements.set(stringLiteralName, []);
         }
+        let idSuffix = this.stringLiteralReplacements
+          .get(stringLiteralName)?.length;
+
+        this.stringLiteralReplacements
+          .get(stringLiteralName)
+          ?.push(stringLiteralName + idSuffix);
+
+        element.innerHTML = element.innerHTML.replace(
+          stringLiteral,
+          `<span id='${stringLiteralName + idSuffix
+          }'></span>`
+        );
       }
     });
     return elements;
   }
 
-  static serializeElements(elements: Element[]) {}
+  static serializeElements(elements: Element[]) { }
 }
