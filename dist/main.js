6fc3bc45ae616802cc456f6585b761f9bcb1099b
Andreas Wänqvist <andrkarl@live.com>
2023-01-04 20:55:26 +0100

    removed onclick

--- dist/main.js
+++ dist/main.js
@@ -280,7 +280,7 @@ class AdvancedComponent extends ElementBase {
 // Define the new element
 customElements.define("advanced-component", AdvancedComponent);
 
-var template = "<div class=\"internal-binding\">\r\n  <button onclick=\"clicked\">Change mode</button>\r\n\r\n  <p>${mode}</p>\r\n</div>\r\n";
+var template = "<div class=\"internal-binding\">\r\n  <!-- <button onclick=\"clicked\">Change mode</button> -->\r\n  <button>Change mode</button>\r\n\r\n  <p>${mode}</p>\r\n</div>\r\n";
 
 var css_248z = ".internal-binding {\r\n    background-color: blueviolet;\r\n    color: aliceblue;\r\n    border: 2px solid white;\r\n    border-radius: 0.5em;\r\n    padding: 8px;\r\n}";
 
